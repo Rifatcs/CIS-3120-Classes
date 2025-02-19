dd80c8d8565986160e419c1e5a91ac7cbd19733c
Rifat <rifatcsahmed@gmail.com>
2025-02-19 00:13:54 -0500

    added cat and its behaviors

--- Animal.py
+++ Animal.py
@@ -1,7 +1,23 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
-
+    def __init__(self,name,eat,sleep,move,bite,swim): 
+        self.name=name
+        self.__eat_behavior=eat
+        self.__sleep_behavior=sleep
+        self.__move_behavior=move
+        self.__bite_behavior=bite
+        self.__swim_behavior=swim
+        
     def talk(self):
-        print("hi")
+        print("cat is purring")
+    def eat(self):
+        print("cat is eating" )
+    def sleep(self):
+        print(" cat is sleeping")
+    def move (self):
+        print("cat is walking")
+    def bite(self):
+        print(" cat is eating " ' "nom nom" ')
+    def swim(self):
+        print("cat is swiming")
+      
 
